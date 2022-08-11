917a8c576e402d9cc0de9d11f426eb29908a96a3
jagadhis <jwaran78@gmail.com>
2022-08-11 14:58:55 +0530

    Container And Styles

---
+++ src/Containers/error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const error = () => (
+  <div>
+    <h1>Page not found</h1>
+    <p>Look for something else</p>
+  </div>
+);
+
+export default error;
---
+++ src/Containers/index.js
@@ -0,0 +1,6 @@
+export { default } from './App';
+export { default as React } from './React';
+export { default as Router } from './Router';
+export { default as id } from './id';
+export { default as Login } from './login';
+export { default as error } from './error';
