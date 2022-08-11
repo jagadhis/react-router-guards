52747b12d87c8429c293cb165ab1d3a5a458ed5c
jagadhis <jwaran78@gmail.com>
2022-08-11 15:00:30 +0530

    Removed Style Files

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'router';
 import { Page } from '../../components';
-import './app.module.scss';
+
 
 const App = () => (
   <Router>{(content, routeProps) => <Page {...routeProps}>{content}</Page>}</Router>
